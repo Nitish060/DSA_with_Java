230c7ff20004654a37740b3d2721f6ca1d075858
nitish <nitishmishra0006@gmail.com>
2024-04-17 17:03:45 +0530

    DSA WITH JAVA

---
+++ Binary_Tree/BT3.java
@@ -0,0 +1,53 @@
+/* Inorder Tree Traversal in Binary Tree */
+
+public class BT3 {
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree
+    {
+        static int idx = -1;
+        public Node buildTree(int nodes[]) //function for building trees
+        {
+            idx++;
+            if(nodes[idx]==-1)
+            {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public void inorder(Node root)//function for inorder traversal
+        {
+            if(root==null)
+            {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; //-1 represents null node
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        tree.inorder(root);
+    }
+}
---
+++ Binary_Tree/BT4.java
@@ -0,0 +1,54 @@
+/* Postorder Tree Traversal of Binary Tree */
+
+public class BT4 {
+    
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree
+    {
+        static int idx = -1;
+        public Node buildTree(int nodes[]) //function for building trees
+        {
+            idx++;
+            if(nodes[idx]==-1)
+            {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public void postorder(Node root)//function for postorder traversal
+        {
+            if(root==null)
+            {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; //-1 represents null node
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        tree.postorder(root);
+    }
+}
